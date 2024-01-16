2cfea70f38cee1b73eafb09ea56449470a05e691
yukimasaki <woodenhoe@gmail.com>
2024-01-16 08:22:19 +0000

    feat: 最大幅を修正

--- frontend/src/app/_components/layout/list-box-wrapper.tsx
+++ frontend/src/app/_components/layout/list-box-wrapper.tsx
@@ -3,7 +3,7 @@ export const ListboxWrapperComponent = ({
 }: {
   children: React.ReactNode,
 }) => (
-  <div className="w-full h-fit max-w-[640px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+  <div className="w-full h-fit border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100 lg:w-1/3">
     {children}
   </div>
 );
