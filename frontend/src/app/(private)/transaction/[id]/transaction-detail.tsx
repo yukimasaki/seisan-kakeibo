461ef8c3bca0771da9fee2e26ca4227e2b9c875c
yukimasaki <woodenhoe@gmail.com>
2024-01-17 06:30:53 +0000

    feat: 詳細ページを実装

--- frontend/src/app/(private)/transaction/[id]/transaction-detail.tsx
+++ frontend/src/app/(private)/transaction/[id]/transaction-detail.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { fetcher } from "@common/fetcher";
+import { Icon } from "@components/icon/icon";
+import { Card, CardHeader, CardBody, CardFooter, Button, Table, TableHeader, TableColumn, TableBody, Spinner, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
 import { Transaction } from "@type/transaction";
-import { usePathname } from "next/navigation";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+import { usePathname, useRouter } from "next/navigation";
 import useSWR from "swr";
 
 export const TransactionDetailComponent = () => {
+  const router = useRouter();
+
   const path = usePathname();
   const id = path.replace("/transaction/", "");
 
@@ -21,8 +27,86 @@ export const TransactionDetailComponent = () => {
     keepPreviousData: true,
   });
 
+  const loadingState = isLoading || transaction.payments.length === 0 ? "loading" : "idle";
+
   return (
-    <>
-    </>
+    <div className="flex flex-col h-svh">
+      <div className="flex-1 p-2 space-y-2">
+        {transaction &&
+          <Card>
+            <CardHeader className="flex">
+              <Button variant="light" onClick={() => router.back()}>
+                <Icon name={"Back"} size={14} />
+                <p>戻る</p>
+              </Button>
+            </CardHeader>
+            <form>
+              <CardBody className="space-y-6 px-6">
+                {/* 1 */}
+                <div className="flex flex-row justify-between">
+                  {/* 1-1 */}
+                  <div className="flex flex-col">
+                    {/* 1-1-1 */}
+                    <div className="flex gap-2">
+                      <Icon name="Groups" className="text-green-500" />
+                      <span className="text-md self-center">{transaction.category.category}</span>
+                    </div>
+                    {/* 1-1-2 */}
+                    <div className="flex gap-2">
+                      <span className="text-md self-center">{dayjs(transaction.paymentDate).locale("ja").format("M/DD (dd)")}</span>
+                    </div>
+                  </div>
+
+                  {/* 1-2 */}
+                  <div className="flex flex-col">
+                    {/* 1-2-1 */}
+                    <div className="flex gap-2 space-x-0.5">
+                      <span className="text-md self-center">{transaction.amount}</span>
+                      <span className="text-md self-center">円</span>
+                    </div>
+                  </div>
+                </div>
+
+                {/* 2 */}
+                <div className="flex flex-col space-y-3">
+                  {/* 2-1 */}
+                  <span className="text-md">{transaction.title}</span>
+                  {/* 2-2 */}
+                  <span className="text-md">{transaction.memo}</span>
+                  {/* 2-3 */}
+                  <Table
+                    aria-label="Example table with dynamic content"
+                    color={"default"}
+                    removeWrapper
+                    isHeaderSticky
+                  >
+                    <TableHeader>
+                      <TableColumn>氏名</TableColumn>
+                      <TableColumn>割り勘</TableColumn>
+                      <TableColumn>立替え</TableColumn>
+                    </TableHeader>
+                    <TableBody
+                      items={transaction.payments ?? []}
+                      loadingState={loadingState}
+                      loadingContent={<Spinner />}
+                    >
+                      {(item) => (
+                        <TableRow key={item.id}>
+                          <TableCell >{item.payer.displayName}</TableCell>
+                          <TableCell >{item.defaultPaymentAmount}円</TableCell>
+                          <TableCell >{item.actualPaymentAmount}円</TableCell>
+                        </TableRow>
+                      )}
+                    </TableBody>
+                  </Table>
+                </div>
+              </CardBody>
+              <CardFooter>
+              </CardFooter>
+            </form>
+          </Card>
+        }
+      </div>
+    </div>
   );
 };
