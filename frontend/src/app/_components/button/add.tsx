167dc7a9dd2cc3b0514f7d224e53225d1d5a032e
yukimasaki <woodenhoe@gmail.com>
2024-01-24 08:50:40 +0000

    feat: AddButtonComponentにonClick関数を渡せるようにした

--- frontend/src/app/_components/button/add.tsx
+++ frontend/src/app/_components/button/add.tsx
@@ -3,13 +3,18 @@
 import { Icon } from "@components/icon/icon";
 import { Button } from "@nextui-org/react";
 
-export const AddButtonComponent = () => {
+export const AddButtonComponent = ({
+  onClickFn,
+}: {
+  onClickFn: () => void,
+}) => {
   return (
     <>
       <Button
         color={"primary"}
         radius={"md"}
         isIconOnly
+        onClick={onClickFn}
       >
         <Icon name="Add" className="text-white" />
       </Button>
