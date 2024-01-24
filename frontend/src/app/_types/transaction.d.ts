e209f1f5752ffdbe33fbe2aba8c9c71f32949408
yukimasaki <woodenhoe@gmail.com>
2024-01-24 07:57:42 +0000

    feat: 作成・更新時のTransaction型定義を作成しファイルを統合

--- frontend/src/app/_types/transaction.d.ts
+++ frontend/src/app/_types/transaction.d.ts
@@ -17,3 +17,44 @@ export type Transaction = {
   payments: Payment[],
   balances: Balance[],
 };
+
+export type CreateTransactionDto = Omit<Transaction,
+  "id",
+  "editorId",
+  "payments",
+  "balances",
+> & PaymentInfo;
+
+export type UpdateTransactionDto = Partial<CreateTransactionDto>;
+
+// 支払情報
+type PaymentInfo = Ratio | Even | AmountBasis;
+
+// 共通
+type Common = {
+  member: {
+    userId: number;
+    finalBill: number;
+  }[];
+}
+
+// 比率
+type Ratio = Common & {
+  tag: "ratio";
+  member: {
+    ratio: number;
+  }[];
+};
+
+// 均等
+type Even = Common & {
+  tag: "even";
+};
+
+// 金額指定
+type AmountBasis = Common & {
+  tag: "amount_basis";
+  member: {
+    specifiedAmount: number;
+  }[];
+};
