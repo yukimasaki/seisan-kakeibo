0dd834b4ef370b615fc792148f5a66bb6cebde15
yukimasaki <woodenhoe@gmail.com>
2024-01-25 06:04:43 +0000

    feat: 入力項目追加、デザイン調整

--- frontend/src/app/(private)/transaction/create-form.tsx
+++ frontend/src/app/(private)/transaction/create-form.tsx
@@ -1,21 +1,46 @@
 "use client";
 
 import useModalForm from "@hooks/useModalForm";
-import { Modal, ModalBody, ModalContent } from "@nextui-org/react";
+import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
 
 export const CreateTransactionForm = () => {
   const form = useModalForm();
+
   return (
     <Modal
       isOpen={form.isOpen}
       onClose={() => form.onClose()}
       placement="center"
+      isDismissable={false}
+      isKeyboardDismissDisabled={true}
+      backdrop="blur"
+      hideCloseButton
     >
       <ModalContent>
-        {(onClose) => (
-          <ModalBody>
-            test
-          </ModalBody>
+        {() => (
+          <>
+            <ModalHeader>
+              <div>
+                新しい支出の登録
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <div className="flex flex-col space-y-4">
+                <Input
+                  label={"金額"}
+                  size="sm"
+                />
+                <Input
+                  label={"タイトル"}
+                  size="sm"
+                />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color={"danger"} variant="light" onPress={() => form.onClose()}>閉じる</Button>
+              <Button color={"primary"} onPress={() => form.onClose()}>作成</Button>
+            </ModalFooter>
+          </>
         )}
       </ModalContent>
     </Modal>
