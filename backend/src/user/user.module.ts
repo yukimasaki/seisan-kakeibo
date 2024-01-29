e06659d231daa1bfb8208a742494bc224995ead2
yukimasaki <woodenhoe@gmail.com>
2024-01-29 04:51:38 +0000

    fix: 依存関係を注入

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '@@nest/common/prisma/prisma.service';
+import { UtilityService } from '@@nest/common/services/utility.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserController],
   providers: [
     UserService,
     PrismaService,
+    JwtService,
+    UtilityService,
   ],
 })
-export class UserModule {}
+export class UserModule { }
