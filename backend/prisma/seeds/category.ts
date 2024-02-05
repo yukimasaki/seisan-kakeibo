00b7f7a03aecae8c1d066e5b8e109679340616da
yukimasaki <woodenhoe@gmail.com>
2024-02-05 07:54:07 +0000

    style: prettier

--- backend/prisma/seeds/category.ts
+++ backend/prisma/seeds/category.ts
@@ -3,22 +3,16 @@ const prisma = new PrismaClient();
 
 export const deleteCategory = async () => {
   await prisma.category.deleteMany();
-}
+};
 
 export const createCategory = async () => {
-  const categoryGroup1 = [
-    `食費`,
-    `日用品`,
-    `通信費`,
-    `交通費`,
-  ];
+  const categoryGroup1 = [`食費`, `日用品`, `通信費`, `交通費`];
 
   await prisma.category.createMany({
-    data: categoryGroup1
-    .map((category, _) => ({
+    data: categoryGroup1.map((category, _) => ({
       icon: `category/icon.png`,
       category,
       groupId: 1,
     })),
   });
-}
+};
