576d98b3e371b5c953daa98241aed022270ee31e
yukimasaki <woodenhoe@gmail.com>
2024-02-08 08:35:41 +0000

    feat: 取引情報作成フォームを作成開始

--- frontend/src/app/(private)/transaction/transaction-create-form.tsx
+++ frontend/src/app/(private)/transaction/transaction-create-form.tsx
@@ -14,18 +14,69 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Select,
+  SelectItem,
+  Tab,
+  Tabs,
   Textarea,
 } from "@nextui-org/react";
 import { Summary } from "@type/calendar";
+import { Category } from "@type/category";
 import { createSummary } from "@utils/createSummary";
+import { useFormState } from "react-dom";
+import { createTransaction, validateOnBlur } from "./transaction-server-action";
+import { useState } from "react";
 
 export const CreateTransactionForm = () => {
+  const [messageAfterSubmit, formAction] = useFormState(createTransaction, {
+    isSubmitted: false,
+    ok: false,
+    message: null,
+    data: null,
+  });
+
   const form = useModalForm();
   const calendarStore = useDatePickerCalendar();
 
   const summaries: Summary[] = createSummary({
     now: calendarStore.currentYearMonth,
   });
+
+  // dummy
+  const categories: Category[] = [
+    { id: 1, icon: "", category: "日用品" },
+    { id: 2, icon: "", category: "光熱費" },
+    { id: 3, icon: "", category: "通信費" },
+    { id: 4, icon: "", category: "食費" },
+    { id: 5, icon: "", category: "外食費" },
+    { id: 6, icon: "", category: "美容費" },
+    { id: 7, icon: "", category: "付き合い" },
+    { id: 8, icon: "", category: "ペット" },
+    { id: 9, icon: "", category: "交通・車両費" },
+    { id: 10, icon: "", category: "住居費" },
+    { id: 11, icon: "", category: "趣味" },
+    { id: 12, icon: "", category: "自己啓発" },
+    { id: 13, icon: "", category: "分類不能" },
+  ];
+
+  const [selectedTab, setSelectedTab] = useState("ratio");
+  const tabs = [
+    {
+      key: "ratio",
+      label: "比率",
+      content: "比率!!",
+    },
+    {
+      key: "even",
+      label: "均等",
+      content: "均等!!",
+    },
+    {
+      key: "amount_basis",
+      label: "金額",
+      content: "金額!!",
+    },
+  ];
 
   return (
     <Modal
@@ -37,66 +88,117 @@ export const CreateTransactionForm = () => {
       backdrop="blur"
       hideCloseButton
     >
-      <ModalContent>
-        {() => (
-          <>
-            <ModalHeader>
-              <div>新しい支出の登録</div>
-            </ModalHeader>
-            <ModalBody>
-              <div className="flex flex-col space-y-4">
-                <Input
-                  label={"金額"}
-                  size="sm"
-                  classNames={{
-                    input: "text-base",
-                  }}
-                />
+      <form action={formAction}>
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader>
+                <div>新しい支出の登録</div>
+              </ModalHeader>
+              <ModalBody>
+                <div className="flex flex-col space-y-4">
+                  <Button
+                    onPress={() => {
+                      validateOnBlur({
+                        tag: selectedTab,
+                        key: "amount",
+                        value: 10000,
+                      });
+                    }}
+                  >
+                    test
+                  </Button>
 
-                <Input
-                  label={"タイトル"}
-                  size="sm"
-                  classNames={{
-                    input: "text-base",
-                  }}
-                />
-
-                <Popover placement={"top"}>
-                  <PopoverTrigger>
-                    <Input
-                      label={"支払日"}
-                      value={calendarStore.selectedDate}
-                      size="sm"
-                      readOnly
-                      classNames={{
-                        input: "text-left text-base",
-                      }}
-                    />
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <CalendarComponent
-                      summaries={summaries}
-                      store={useDatePickerCalendar}
-                    />
-                  </PopoverContent>
-                </Popover>
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                color={"danger"}
-                variant="light"
-                onPress={() => form.onClose()}
-              >
-                閉じる
-              </Button>
-              <Button color={"primary"} onPress={() => form.onClose()}>
-                作成
-              </Button>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
+                  <Input
+                    label={"金額"}
+                    name={"amount"}
+                    inputMode={"numeric"}
+                    size={"sm"}
+                    classNames={{
+                      input: "text-base",
+                    }}
+                  />
+                  <Select
+                    label={"カテゴリー"}
+                    name={"categoryId"}
+                    items={categories}
+                    placeholder={"カテゴリーを選択"}
+                  >
+                    {(category) => (
+                      <SelectItem key={category.id}>
+                        {category.category}
+                      </SelectItem>
+                    )}
+                  </Select>
+                  <Input
+                    label={"タイトル"}
+                    name={"title"}
+                    size={"sm"}
+                    classNames={{
+                      input: "text-base",
+                    }}
+                  />
+                  <Popover placement={"top"}>
+                    <PopoverTrigger>
+                      <Input
+                        label={"支払日"}
+                        name={"paymentDate"}
+                        value={calendarStore.selectedDate}
+                        size="sm"
+                        readOnly
+                        classNames={{
+                          input: "text-left",
+                        }}
+                      />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <CalendarComponent
+                        summaries={summaries}
+                        store={useDatePickerCalendar}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <Tabs
+                    items={tabs}
+                    onSelectionChange={(tab) => setSelectedTab(tab.toString())}
+                  >
+                    {(tab) => (
+                      <Tab key={tab.key} title={tab.label}>
+                        <input name={"tag"} value={tab.key} hidden readOnly />
+                        <div>{tab.content}</div>
+                      </Tab>
+                    )}
+                  </Tabs>
+                  <Textarea
+                    label={"備考"}
+                    name={"memo"}
+                    classNames={{
+                      input: "text-base",
+                    }}
+                  />
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color={"danger"}
+                  variant="light"
+                  onPress={() => form.onClose()}
+                >
+                  閉じる
+                </Button>
+                <Button
+                  type={"submit"}
+                  color={"primary"}
+                  variant={"flat"}
+                  onPress={() => form.onClose()}
+                >
+                  作成
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </form>
     </Modal>
   );
 };
--- frontend/src/app/(private)/transaction/transaction-server-action.tsx
+++ frontend/src/app/(private)/transaction/transaction-server-action.tsx
@@ -1,5 +1,7 @@
 "use server";
 
+import { authOptions } from "@common/next-auth/options";
+import { ServerActionResult } from "@type/server-actions";
 import {
   AmountBasis,
   Common,
@@ -7,8 +9,12 @@ import {
   Even,
   Ratio,
 } from "@type/transaction";
-import { z } from "zod";
+import { getServerSession } from "next-auth";
+import React from "react";
+import { ZodError, z } from "zod";
 
+// **************************** スキーマ ****************************
+// オブジェクト単位
 const CommonSchema = z.object({
   creatorId: z.number(), // session
   amount: z.number(), // formData
@@ -57,3 +63,101 @@ const CreateTransactionSchema = z.discriminatedUnion("tag", [
   EvenSchema,
   AmountBasisSchema,
 ]) satisfies z.ZodType<CreateTransactionDto>;
+
+// **************************** Server Actions ****************************
+export const createTransaction = async (
+  prevState: {
+    message: string | null;
+  },
+  formData: FormData
+): Promise<ServerActionResult<CreateTransactionDto>> => {
+  const session = await getServerSession(authOptions);
+  const token = session?.user.accessToken;
+
+  const creatorId = session?.profile?.id;
+  const groupId = session?.activeGroup?.id;
+
+  const tag = formData.get("tag");
+  const amount = formData.get("amount");
+  const paymentDate = formData.get("paymentDate");
+  const title = formData.get("title");
+  const memo = formData.get("memo");
+  const categoryId = formData.get("categoryId");
+
+  // const createTransactionDto: CreateTransactionDto = {
+  //   tag,
+  //   creatorId,
+  //   amount,
+  //   paymentDate,
+  //   title,
+  //   memo,
+  //   categoryId,
+  //   groupId,
+  //   // members,
+  // };
+
+  return {
+    isSubmitted: true,
+    ok: true,
+    message: "test",
+    data: null,
+  };
+};
+
+type RatioSchemaKeys = (typeof RatioSchema)["_type"];
+type EvenSchemaKeys = (typeof EvenSchema)["_type"];
+type AmountBasisSchemaKeys = (typeof AmountBasisSchema)["_type"];
+export const validateOnBlur = ({
+  tag,
+  key,
+  value,
+}: {
+  tag: React.Key;
+  key:
+    | keyof RatioSchemaKeys
+    | keyof EvenSchemaKeys
+    | keyof AmountBasisSchemaKeys;
+  value: unknown;
+}): {
+  message: string | null;
+} => {
+  const schema = (() => {
+    switch (tag) {
+      case "ratio": {
+        const schema = RatioSchema.pick({ [key]: true });
+        return schema;
+      }
+      case "even": {
+        const schema = EvenSchema.pick({ [key]: true });
+        return schema;
+      }
+      case "amount_basis": {
+        const schema = AmountBasisSchema.pick({ [key]: true });
+        return schema;
+      }
+    }
+  })();
+
+  if (!schema)
+    return {
+      message: null,
+    };
+
+  try {
+    schema.parse({ [key]: value });
+    return {
+      message: null,
+    };
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ZodError) {
+      return {
+        message: error.errors[0].message,
+      };
+    } else {
+      return {
+        message: null,
+      };
+    }
+  }
+};
