95643c847baf39cbc5465e34ff610f6c204afa53
yukimasaki <woodenhoe@gmail.com>
2024-02-08 05:02:17 +0000

    feat: HomePageとRootPageを分割

---
+++ frontend/src/app/(public)/home/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const HomePage = () => {
+  return <div>ここがホームページだ！</div>;
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,56 +1,18 @@
 "use server";
 
 import { NavbarComponent } from "@components/navbar/navbar";
-import { Icon } from "@components/icon/icon";
-import { PageListComponent } from "@components/page-list";
 import { SwitchButton } from "@components/navbar/switch-button";
+import { HomePage } from "./(public)/home/page";
 
-const HomePage = async () => {
-  const iconClasses = "text-xl text-blue-500 pointer-events-none flex-shrink-0";
-
-  const pages = [
-    {
-      name: "Home",
-      path: "/",
-      icon: Icon({
-        name: "Groups",
-        className: iconClasses,
-      }),
-    },
-    {
-      name: "Profile",
-      path: "/profile",
-      icon: Icon({
-        name: "Groups",
-        className: iconClasses,
-      }),
-    },
-    {
-      name: "Group",
-      path: "/group",
-      icon: Icon({
-        name: "Groups",
-        className: iconClasses,
-      }),
-    },
-    {
-      name: "Transaction",
-      path: "/transaction",
-      icon: Icon({
-        name: "Groups",
-        className: iconClasses,
-      }),
-    },
-  ];
-
+const RootPage = async () => {
   return (
     <>
       <NavbarComponent>
         <SwitchButton />
       </NavbarComponent>
-      <PageListComponent pages={pages} />
+      <HomePage />
     </>
   );
 };
 
-export default HomePage;
+export default RootPage;
