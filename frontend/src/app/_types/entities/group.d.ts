dbf791611115b06031aecfc9fa9a7b17fc45d919
yukimasaki <woodenhoe@gmail.com>
2024-02-12 14:52:08 +0000

    feat: Group型定義を更新

--- frontend/src/app/_types/entities/group.d.ts
+++ frontend/src/app/_types/entities/group.d.ts
@@ -1,6 +1,17 @@
+import { Member } from "@type/entities/member";
+
 export type Group = {
   id: number;
   uuid: string;
   displayName: string;
+};
+
+type AddtionalGroupInfo = {
   members: Member[];
 };
+
+export type GroupResponse = Group & AddtionalGroupInfo;
+
+export type CreateGroupDto = Omit<Group, "id">;
+
+export type UpdateGroupDto = Partial<Group>;
