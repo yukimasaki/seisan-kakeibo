ff685bb40e1c1d9c6dce0e541fa3714ca19b3eb1
yukimasaki <woodenhoe@gmail.com>
2024-02-14 02:33:26 +0000

    feat: FinalBillにhandleChange、handleClearを実装

--- frontend/src/app/(private)/transaction/form/FinalBill.tsx
+++ frontend/src/app/(private)/transaction/form/FinalBill.tsx
@@ -2,13 +2,31 @@
 
 import { ParagraphComponent } from "@components/text/paragraph";
 import { Input } from "@nextui-org/react";
-import { MemberResponse } from "@type/entities/member";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-export const FinalBillComponent = ({
-  members,
-}: {
-  members: MemberResponse[];
-}) => {
+export const FinalBillComponent = () => {
+  const { data: session } = useSession();
+  const members = session?.profile.activeGroup.members;
+  if (!members) return null;
+
+  // input
+  const [finalBills, setFinalBills] = useState(() => {
+    return members.map(() => "");
+  });
+
+  const handleChange = (idx: number, value: string) => {
+    const newFinalBills = [...finalBills];
+    newFinalBills[idx] = value;
+    setFinalBills(newFinalBills);
+  };
+
+  const handleClear = (idx: number) => {
+    const newFinalBills = [...finalBills];
+    newFinalBills[idx] = "";
+    setFinalBills(newFinalBills);
+  };
+
   return (
     <>
       <input name={"memberCount"} value={members.length} readOnly hidden />
@@ -20,8 +38,12 @@ export const FinalBillComponent = ({
           <div className="flex flex-row space-x-2 items-center">
             <Input
               name={`member.${idx}.finalBill`}
+              value={finalBills[idx]}
               size={"sm"}
               variant="underlined"
+              onChange={(e) => handleChange(idx, e.target.value)}
+              onClear={() => handleClear(idx)}
+              isClearable
             />
             <ParagraphComponent>円</ParagraphComponent>
           </div>
