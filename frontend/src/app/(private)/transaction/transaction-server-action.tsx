bd98b10e77b55da90f375b98a9bb9e626da75576
yukimasaki <woodenhoe@gmail.com>
2024-02-14 06:48:39 +0000

    feat: 全パターンのバリデーションを実装

--- frontend/src/app/(private)/transaction/transaction-server-action.tsx
+++ frontend/src/app/(private)/transaction/transaction-server-action.tsx
@@ -29,9 +29,9 @@ const CommonInputSchema = z.object({
 const CreateTransactionSchema = CommonInputSchema.extend({
   member: z.array(
     z.object({
-      userId: z.coerce.number(),
-      finalBill: z.coerce.number(),
-      balance: z.coerce.number(),
+      userId: z.number(),
+      finalBill: z.number(),
+      balance: z.number(),
     })
   ),
 }) satisfies z.ZodType<CreateTransactionDto>;
@@ -49,17 +49,21 @@ export const createTransaction = async (
   const groupId = session?.profile.activeGroup?.id;
   const status = "未精算";
 
-  const amount = formData.get("amount");
-  const categoryId = formData.get("categoryId");
+  const amount = Number(formData.get("amount"));
+  const categoryId = Number(formData.get("categoryId"));
   const title = formData.get("title");
-  const tag = formData.get("tag");
-  const paymentDate = formData.get("paymentDate");
+  const tag = formData.get("tag") as PaymentType;
+  const paymentDate = (() => {
+    const paymentDate = formData.get("paymentDate")?.toString();
+    if (!paymentDate) return new Date();
+    return new Date(paymentDate);
+  })();
   const memo = formData.get("memo");
   const memberCount = formData.get("memberCount");
   const member = Array.from({ length: Number(memberCount) }, (_, idx) => ({
-    userId: formData.get(`member.${idx}.userId`),
-    finalBill: formData.get(`member.${idx}.finalBill`),
-    balance: formData.get(`member.${idx}.balance`),
+    userId: Number(formData.get(`member.${idx}.userId`)),
+    finalBill: Number(formData.get(`member.${idx}.finalBill`)),
+    balance: Number(formData.get(`member.${idx}.balance`)),
   }));
 
   const createTransactionDto = {
@@ -77,16 +81,58 @@ export const createTransaction = async (
 
   console.log(createTransactionDto);
 
+  // 1. 入力値が型として正しいかどうかのバリデーション
   try {
     CreateTransactionSchema.parse(createTransactionDto);
   } catch (error) {
     console.log(error);
   }
 
+  // 2. 支払い情報が不釣り合いではないかのバリデーション
+  const initialValue = 0;
+  const totalFinalBill = member.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.finalBill,
+    initialValue
+  );
+
+  if (totalFinalBill !== amount) {
+    console.log("amountとtotalFinalBillが一致していません");
+    return {
+      isSubmitted: true,
+      ok: false,
+      message: "amountとtotalFinalBillが一致していません",
+    };
+  }
+
+  const totalBalance = member.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.balance,
+    initialValue
+  );
+  if (tag === "ratio") {
+    if (totalBalance !== 100) {
+      console.log("ratioの合計が100%ではありません");
+      return {
+        isSubmitted: true,
+        ok: false,
+        message: "ratioの合計が100%ではありません",
+      };
+    }
+  } else {
+    if (totalBalance !== amount) {
+      console.log("amountとtotalBalanceが一致していません");
+      return {
+        isSubmitted: true,
+        ok: false,
+        message: "amountとtotalBalanceが一致していません",
+      };
+    }
+  }
+
+  // 3. すべてのバリデーションに通過した場合、成功のメッセージを返す
   return {
     isSubmitted: true,
     ok: true,
-    message: "test",
+    message: "保存しました",
   };
 };
 
