8f81f3797c4bcdf041b1ed6038bc8dc73a87d91c
yukimasaki <woodenhoe@gmail.com>
2024-02-14 02:27:24 +0000

    feat: handleChange、handleClearを実装

--- frontend/src/app/(private)/transaction/form/BalanceInput.tsx
+++ frontend/src/app/(private)/transaction/form/BalanceInput.tsx
@@ -2,20 +2,36 @@
 
 import { ParagraphComponent } from "@components/text/paragraph";
 import { Input } from "@nextui-org/react";
-import { MemberResponse } from "@type/entities/member";
 import { PaymentType } from "../transaction-server-action";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 
-export const BalanceInputComponent = ({
-  members,
-  tag,
-}: {
-  members: MemberResponse[];
-  tag: PaymentType;
-}) => {
+export const BalanceInputComponent = ({ tag }: { tag: PaymentType }) => {
+  const { data: session } = useSession();
+  const members = session?.profile.activeGroup.members;
+  if (!members) return null;
+
   const unit = (() => {
     if (tag === "ratio") return "%";
     return "円";
   })();
+
+  // input
+  const [balances, setBalances] = useState(() => {
+    return members.map(() => "");
+  });
+
+  const handleChange = (idx: number, value: string) => {
+    const newBalance = [...balances];
+    newBalance[idx] = value;
+    setBalances(newBalance);
+  };
+
+  const handleClear = (idx: number) => {
+    const newBalance = [...balances];
+    newBalance[idx] = "";
+    setBalances(newBalance);
+  };
 
   return (
     <>
@@ -28,8 +44,12 @@ export const BalanceInputComponent = ({
           <div className="flex flex-row space-x-2 items-center">
             <Input
               name={`member.${idx}.balance`}
+              value={balances[idx]}
               size={"sm"}
               variant="underlined"
+              onChange={(e) => handleChange(idx, e.target.value)}
+              onClear={() => handleClear(idx)}
+              isClearable
             />
             <ParagraphComponent>{unit}</ParagraphComponent>
           </div>
