cf158c5f33d16df6246c3494ea9768af1373ca45
yukimasaki <woodenhoe@gmail.com>
2024-02-15 13:54:30 +0000

    feat: フロントエンドと共通の型定義に基づく処理へ修正

--- backend/src/transaction/transaction.service.ts
+++ backend/src/transaction/transaction.service.ts
@@ -1,74 +1,96 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTransactionFormData } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { PrismaService } from '@@nest/common/prisma/prisma.service';
 import { CreatePaymentDto } from '@@nest/payment/dto/create-payment.dto';
 import { CreateBalanceDto } from '@@nest/balance/dto/create-balance.dto';
 import { TransactionResponse } from './entities/transaction.entity';
 import * as dayjs from 'dayjs';
+import { CreateTransactionDto } from '@@nest/transaction/dto/create-transaction.dto';
 
 @Injectable()
 export class TransactionService {
-  constructor(
-    private readonly prisma: PrismaService,
-  ) { }
+  constructor(private readonly prisma: PrismaService) {}
 
-  async createWithTransaction(
-    createTransactionFormData: CreateTransactionFormData,
-  ) {
-
+  async createWithTransaction(createTransactionDto: CreateTransactionDto) {
     // 1. Prismaのトランザクション処理を開始
     return await this.prisma.$transaction(async (prisma) => {
       // 2. Transactionを作成
-      const {
-        method,
-        paymentInfoArray,
-        ...createTransactionDto
-      } = createTransactionFormData;
+      const { member, ...commonInput } = createTransactionDto;
 
-      const transaction: TransactionResponse = await this.prisma.transaction.create({
-        data: createTransactionDto,
-      });
+      const transaction: TransactionResponse =
+        await this.prisma.transaction.create({
+          data: commonInput,
+        });
 
-      // todo: フロントエンドから渡されてきたCreateTransactionFormDataオブジェクトをトランザクション処理に利用できる形式に変換する処理を書く
+      // todo: フロントエンドから渡されてきたcreateTransactionDtoをトランザクション処理に利用できる形式に変換する処理を書く
       // 3. transactionIdを取得
       const transactionId = transaction.id;
 
       // 4. CreatePaymentDto[]を作成
-      const totalActualPaymentAmount: number = createTransactionFormData.amount;
-      const createPaymentDto: CreatePaymentDto[] = createTransactionFormData.paymentInfoArray.map(
-        paymentInfo => ({
-          payerId: paymentInfo.userId,
-          actualPaymentAmount: paymentInfo.amountEachMember,
-          defaultPaymentAmount: Math.round(totalActualPaymentAmount * paymentInfo.ratio),
-          difference: (paymentInfo.amountEachMember) - Math.round(totalActualPaymentAmount * paymentInfo.ratio),
-          method: createTransactionFormData.method,
-          ratio: paymentInfo.ratio,
-          transactionId,
-        }));
+      const totalAmount = createTransactionDto.amount;
+      const method = createTransactionDto.method;
+
+      const createPaymentDto: CreatePaymentDto[] = (() => {
+        switch (method) {
+          case 'ratio':
+            return createTransactionDto.member.map((eachMember) => {
+              const actualPaymentAmount = eachMember.finalBill;
+              const defaultPaymentAmount = Math.round(
+                totalAmount * (eachMember.balance / 100),
+              );
+
+              return {
+                payerId: eachMember.userId,
+                actualPaymentAmount,
+                defaultPaymentAmount,
+                difference: actualPaymentAmount - defaultPaymentAmount,
+                method,
+                ratio: eachMember.balance / 100,
+                transactionId,
+              };
+            }) satisfies CreatePaymentDto[];
+          default:
+            // todo: ratio以外の場合の処理を書く
+            return createTransactionDto.member.map((eachMember) => {
+              const actualPaymentAmount = eachMember.finalBill;
+              const defaultPaymentAmount = eachMember.balance;
+
+              return {
+                payerId: eachMember.userId,
+                actualPaymentAmount,
+                defaultPaymentAmount,
+                difference: actualPaymentAmount - defaultPaymentAmount,
+                method,
+                ratio: eachMember.balance / 100,
+                transactionId,
+              };
+            }) satisfies CreatePaymentDto[];
+        }
+      })();
 
       // 5. CreateBalanceDto[]を作成
       // 規定額より支払いが多いユーザーを抽出
-      const highPaymentUsers = createPaymentDto.filter(payment =>
-        payment.actualPaymentAmount > payment.defaultPaymentAmount
+      const highPaymentUsers = createPaymentDto.filter(
+        (payment) => payment.actualPaymentAmount > payment.defaultPaymentAmount,
       );
 
       // 規定額より支払いが少ないユーザーを抽出
-      const lowPaymentUsers = createPaymentDto.filter(payment =>
-        payment.actualPaymentAmount < payment.defaultPaymentAmount
+      const lowPaymentUsers = createPaymentDto.filter(
+        (payment) => payment.actualPaymentAmount < payment.defaultPaymentAmount,
       );
 
       // 支払いが多いユーザー・支払いが少ないユーザーごとにループ処理で賃借記録を作成する
-      const createBalanceDto: CreateBalanceDto[] =
-        lowPaymentUsers.map(lowPaymentUser => {
-          return highPaymentUsers.map(highPaymentUser => ({
+      const createBalanceDto: CreateBalanceDto[] = lowPaymentUsers
+        .map((lowPaymentUser) => {
+          return highPaymentUsers.map((highPaymentUser) => ({
             lenderId: highPaymentUser.payerId,
             borrowerId: lowPaymentUser.payerId,
             amount: Math.abs(lowPaymentUser.difference),
             status: `未精算`,
             transactionId,
           }));
-        }).flat();
+        })
+        .flat();
 
       await Promise.all([
         // 6. Paymentを作成
@@ -79,11 +101,10 @@ export class TransactionService {
         await this.prisma.balance.createMany({
           data: createBalanceDto,
         }),
-      ])
-        .catch(err => {
-          console.log(`トランザクション処理に失敗しました`);
-          console.log(err);
-        });
+      ]).catch((err) => {
+        console.log(`トランザクション処理に失敗しました`);
+        console.log(err);
+      });
 
       return transaction;
     });
@@ -97,13 +118,10 @@ export class TransactionService {
   //   });
   // }
 
-  async findByPaymentDate(
-    start: string,
-    end: string,
-  ) {
+  async findByPaymentDate(start: string, end: string) {
     const jst = -9;
-    const queryStart = dayjs(start).add(jst, "hour").toDate();
-    const queryEnd = dayjs(end).add(jst, "hour").toDate();
+    const queryStart = dayjs(start).add(jst, 'hour').toDate();
+    const queryEnd = dayjs(end).add(jst, 'hour').toDate();
 
     const transactions = await this.prisma.transaction.findMany({
       include: {
@@ -140,13 +158,13 @@ export class TransactionService {
   async update(id: number, updateTransactionDto: UpdateTransactionDto) {
     return await this.prisma.transaction.update({
       where: { id },
-      data: updateTransactionDto
+      data: updateTransactionDto,
     });
   }
 
   async remove(id: number) {
     return await this.prisma.transaction.delete({
-      where: { id }
+      where: { id },
     });
   }
 }
