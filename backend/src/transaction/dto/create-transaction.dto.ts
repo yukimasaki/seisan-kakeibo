7bfb27494f0c94d027d68e8b0b60de9181d9b22f
yukimasaki <woodenhoe@gmail.com>
2024-02-15 12:47:00 +0000

    feat: フロントエンドの型定義と合わせた

--- backend/src/transaction/dto/create-transaction.dto.ts
+++ backend/src/transaction/dto/create-transaction.dto.ts
@@ -1,13 +1,20 @@
-import { Transaction } from "../entities/transaction.entity";
-import { OmitType } from "@nestjs/swagger";
+import { Transaction } from '../entities/transaction.entity';
+import { IntersectionType, OmitType } from '@nestjs/swagger';
 
-export class CreateTransactionFormData extends OmitType(Transaction, ['id', 'editorId']) {
-  method: string;
-  paymentInfoArray: {
+type PaymentType = 'ratio' | 'even' | 'amount_basis';
+
+class CommonInput extends OmitType(Transaction, ['id', 'editorId']) {}
+
+class BalanceInput {
+  method: PaymentType;
+  member: {
     userId: number;
-    ratio: number;
-    amountEachMember: number;
+    finalBill: number;
+    balance: number;
   }[];
 }
 
-export class CreateTransactionDto extends OmitType(Transaction, ['id', 'editorId']) { }
+export class CreateTransactionDto extends IntersectionType(
+  CommonInput,
+  BalanceInput,
+) {}
