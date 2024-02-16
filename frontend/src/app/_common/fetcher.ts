c7aed0c13f2c33ef941cb98cb83a0b437d4295fd
yukimasaki <woodenhoe@gmail.com>
2024-02-16 02:35:10 +0000

    feat: tokenをオプショナルな第2引数として追記

--- frontend/src/app/_common/fetcher.ts
+++ frontend/src/app/_common/fetcher.ts
@@ -1,5 +1,8 @@
-export const fetcher = (
-  url: string,
-) => fetch(
-  url,
-).then(res => res.json());
+export const fetcher = (url: string, token?: string) => {
+  if (!token) return fetch(url).then((res) => res.json());
+  return fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+};
