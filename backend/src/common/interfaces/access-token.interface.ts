b6d7bb955b8b41563110ebb4127378ffd79c605e
yukimasaki <woodenhoe@gmail.com>
2024-02-16 02:28:35 +0000

    feat: フロントエンドのJWT型定義と合わせる対応

--- backend/src/common/interfaces/access-token.interface.ts
+++ backend/src/common/interfaces/access-token.interface.ts
@@ -1,3 +1,15 @@
+import { User, UserResponse } from '@@nest/user/entities/user.entity';
+
 export class AccessToken {
+  name: string;
+  email: string;
   sub: string;
+  idToken: string;
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpired: number;
+  refreshTokenExpired: number;
+  user: User;
+  error: string;
+  profile: UserResponse;
 }
