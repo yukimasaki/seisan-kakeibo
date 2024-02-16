9573208bbb8e5f9e680d88a8d171f9586de57da1
yukimasaki <woodenhoe@gmail.com>
2024-02-16 02:46:39 +0000

    fix: 依存性注入

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { PrismaService } from '@@nest/common/prisma/prisma.service';
+import { UtilityService } from '@@nest/common/services/utility.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [CategoryController],
-  providers: [
-    CategoryService,
-    PrismaService,
-  ],
+  providers: [CategoryService, PrismaService, JwtService, UtilityService],
 })
 export class CategoryModule {}
