c2182ed86a09f2786142736ceab75a7db75a7304
yukimasaki <woodenhoe@gmail.com>
2024-02-23 16:31:56 +0000

    feat: Swaggerドキュメント記述、不要なエンドポイントを削除

--- backend/src/invite/invite.controller.ts
+++ backend/src/invite/invite.controller.ts
@@ -1,41 +1,53 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { InviteService } from './invite.service';
 import { CreateInviteDto } from './dto/create-invite.dto';
-import { UpdateInviteDto } from './dto/update-invite.dto';
+import {
+  ApiOperation,
+  ApiProduces,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { SummarizeApiResponse } from '@decorators/summarize-api-response.decorator';
+import { InviteResponse } from './entities/invite.entity';
 
-@Controller('invite')
+@Controller('invites')
+@ApiTags('/invites')
+@SummarizeApiResponse()
 export class InviteController {
   constructor(private readonly inviteService: InviteService) {}
 
   @Post()
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: 'メンバー招待情報生成API' })
+  @ApiResponse({
+    status: 201,
+    description: '生成したトークンを返却',
+    type: InviteResponse,
+  })
   create(@Body() createInviteDto: CreateInviteDto) {
     return this.inviteService.create(createInviteDto);
   }
 
-  @Get()
-  findAll() {
-    return this.inviteService.findAll();
-  }
-
   @Get(':key')
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: 'グループID取得API' })
+  @ApiResponse({
+    status: 201,
+    description: '指定したトークンに対応するグループIDを返却',
+    type: InviteResponse,
+  })
   findOne(@Param('key') key: string) {
     return this.inviteService.findOne(key);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInviteDto: UpdateInviteDto) {
-    return this.inviteService.update(+id, updateInviteDto);
-  }
-
   @Delete(':key')
+  @ApiProduces('application/json; charset=utf-8')
+  @ApiOperation({ summary: 'メンバー招待情報削除API' })
+  @ApiResponse({
+    status: 201,
+    description: '削除後のメンバー招待情報を返却',
+    type: InviteResponse,
+  })
   remove(@Param('key') key: string) {
     return this.inviteService.remove(key);
   }
