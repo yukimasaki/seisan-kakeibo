c324cd45d5ae0c7322ed23ac2583024102608244
yukimasaki <woodenhoe@gmail.com>
2024-02-23 02:46:18 +0000

    fix: 依存性注入

--- backend/src/invite/invite.module.ts
+++ backend/src/invite/invite.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InviteService } from './invite.service';
 import { InviteController } from './invite.controller';
+import { RedisService } from '@@nest/common/redis/redis.service';
 
 @Module({
   controllers: [InviteController],
-  providers: [InviteService],
+  providers: [InviteService, RedisService],
 })
 export class InviteModule {}
