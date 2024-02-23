79358882c6029e7b18e9c0894dc745733963bb9a
yukimasaki <woodenhoe@gmail.com>
2024-02-23 02:05:55 +0000

    feat: RedisServiceを作成

--- backend/src/common/redis/redis.service.ts
+++ backend/src/common/redis/redis.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Redis } from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  private readonly redis = new Redis({
+    host: 'redis-container',
+    port: 6379,
+  });
+
+  async findOne(key: string): Promise<string> {
+    return await this.redis.get(key);
+  }
+
+  async setValue(key: string, value: string, expiresIn: number) {
+    return await this.redis.set(key, value, 'EX', expiresIn);
+  }
+
+  async delete(key: string): Promise<number> {
+    return await this.redis.del(key);
+  }
+}
