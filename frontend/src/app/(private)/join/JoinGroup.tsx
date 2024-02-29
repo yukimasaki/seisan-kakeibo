46cf125dbfd53d308d5df7884a23e7a29004c10f
yukimasaki <woodenhoe@gmail.com>
2024-02-29 06:13:19 +0000

    feat: グループに参加する処理を追加

--- frontend/src/app/(private)/join/JoinGroup.tsx
+++ frontend/src/app/(private)/join/JoinGroup.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import { FetchError, fetcher } from "@common/fetcher";
-import { Spinner } from "@nextui-org/react";
-import { InviteResponse } from "@type/entities/invite";
+import { PositionCenterWrapperComponent } from "@components/layout/position-center-wrapper";
+import { ParagraphComponent } from "@components/text/paragraph";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Spinner,
+} from "@nextui-org/react";
+import { FindInviteResponse } from "@type/entities/invite";
 import { useSearchParams } from "next/navigation";
+import { useFormState } from "react-dom";
 import useSWR from "swr";
+import { joinGroup } from "./join-server-action";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { showToast } from "@components/toast/toast";
 
 export const JoinGroup = () => {
   const searchParams = useSearchParams();
@@ -15,7 +29,7 @@ export const JoinGroup = () => {
     error,
     isLoading,
   }: {
-    data: InviteResponse;
+    data: FindInviteResponse;
     error: FetchError | undefined;
     isLoading: boolean;
   } = useSWR(
@@ -32,7 +46,65 @@ export const JoinGroup = () => {
     }
   );
 
+  const [messageAfterSubmit, formAction] = useFormState(joinGroup, {
+    isSubmitted: false,
+    ok: false,
+    message: null,
+  });
+
+  const { update } = useSession();
+  useEffect(() => {
+    if (messageAfterSubmit.isSubmitted && messageAfterSubmit.ok) {
+      update();
+      showToast({
+        message: messageAfterSubmit.message || "",
+        type: "success",
+        timerProgressBar: true,
+        timer: 5000,
+      });
+    } else if (messageAfterSubmit.isSubmitted && !messageAfterSubmit.ok) {
+      showToast({
+        message: messageAfterSubmit.message || "",
+        type: "error",
+        timerProgressBar: true,
+        timer: 5000,
+      });
+    }
+    messageAfterSubmit.isSubmitted = false;
+  }, [messageAfterSubmit]);
+
   if (isLoading) return <Spinner />;
 
-  return <div>{inviteResponse.groupId}</div>;
+  return (
+    <PositionCenterWrapperComponent>
+      <Card className="flex justify-between w-full h-fit max-w-[500px]">
+        <CardHeader className="flex flex-col items-center gap-3">
+          <h5 className={"text-blue-400 font-bold text-3xl pt-4"}>
+            {inviteResponse.group.displayName}
+          </h5>
+        </CardHeader>
+        <form action={formAction}>
+          <CardBody className="space-y-4 items-center">
+            <ParagraphComponent>
+              作成者: {inviteResponse.group.creator.userName}
+            </ParagraphComponent>
+            <input
+              name={"groupId"}
+              value={inviteResponse.groupId}
+              hidden
+              readOnly
+            />
+          </CardBody>
+          <CardFooter className="justify-center space-x-4 pb-4">
+            <Button color={"danger"} variant={"light"}>
+              拒否する
+            </Button>
+            <Button type={"submit"} color={"primary"}>
+              参加する
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </PositionCenterWrapperComponent>
+  );
 };
---
+++ frontend/src/app/(private)/join/join-server-action.tsx
@@ -0,0 +1,65 @@
+"use server";
+
+import { authOptions } from "@common/next-auth/options";
+import { CreateMemberDto } from "@type/entities/member";
+import { ServerActionResult } from "@type/server-actions";
+import { getServerSession } from "next-auth";
+
+export const joinGroup = async (
+  prevState: {
+    message: string | null;
+  },
+  formData: FormData
+): Promise<ServerActionResult> => {
+  const session = await getServerSession(authOptions);
+  const token = session?.user.accessToken;
+
+  if (!session)
+    return {
+      isSubmitted: true,
+      ok: false,
+      message: "セッション取得エラー",
+    };
+
+  const userId = session?.profile.id;
+  const groupId = formData.get("groupId") as string;
+
+  if (!groupId)
+    return {
+      isSubmitted: true,
+      ok: false,
+      message: "グループIDが不正です",
+    };
+
+  const createMemberDto: CreateMemberDto = {
+    userId,
+    groupId: parseInt(groupId),
+  };
+
+  console.log(createMemberDto);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/members`,
+    {
+      method: "POST",
+      body: JSON.stringify(createMemberDto),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok)
+    return {
+      isSubmitted: true,
+      ok: false,
+      message: "APIリクエストに失敗しました",
+    };
+
+  return {
+    isSubmitted: true,
+    ok: true,
+    message: "グループに参加しました",
+  };
+};
