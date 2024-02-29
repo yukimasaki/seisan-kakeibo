9190ba0651808e5e9dc15fe5c4ea35b04c4c30b5
yukimasaki <woodenhoe@gmail.com>
2024-02-29 02:04:12 +0000

    fix: 返り値をInviteResponse型に修正

--- backend/src/invite/invite.service.ts
+++ backend/src/invite/invite.service.ts
@@ -1,15 +1,15 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { UpdateInviteDto } from './dto/update-invite.dto';
 import { RedisService } from 'src/common/redis/redis.service';
 import { CreateRedisRecordDto } from 'src/common/redis/dto/create-redis.dto';
 import { v4 as uuid } from 'uuid';
 import { CreateInviteDto } from './dto/create-invite.dto';
+import { InviteResponse } from './entities/invite.entity';
 
 @Injectable()
 export class InviteService {
   constructor(private readonly redisService: RedisService) {}
 
-  async create(createInviteDto: CreateInviteDto): Promise<string> {
+  async create(createInviteDto: CreateInviteDto): Promise<InviteResponse> {
     const token = uuid();
     const { groupId } = createInviteDto;
 
@@ -21,7 +21,7 @@ export class InviteService {
 
     try {
       await this.redisService.setValue(createRedisRecordDto);
-      return token;
+      return { token };
     } catch (error) {
       console.log(error);
       throw new BadRequestException();
