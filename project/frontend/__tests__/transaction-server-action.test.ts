9931704b3960f7d4f7cdeca07a11f85afc6e0e93
yukimasaki <woodenhoe@gmail.com>
2024-03-18 15:53:39 +0000

    test: createMock関数を上書きできるよう変更した

--- project/frontend/__tests__/transaction-server-action.test.ts
+++ project/frontend/__tests__/transaction-server-action.test.ts
@@ -2,42 +2,58 @@ import { CreateTransactionComplex, Member } from "@dto/create-transaction.dto";
 import { PaymentMethod, PaymentStatus } from "@entity/transaction.entity";
 import { extractFormData } from "../src/app/(private)/transaction/server-actions";
 
-const creatorId: number = 1;
-const groupId: number = 1;
-const status: PaymentStatus = "PENDING";
-
-const createMock = (): FormData => {
+const createMock = (
+  overrides?: Partial<CreateTransactionComplex>,
+): FormData => {
   const formData: FormData = new FormData();
 
-  const method: PaymentMethod = "RATIO";
-  const amount: number = 1000;
-  const categoryId: number = 1;
-  const title: string = "テスト";
-  const paymentDate: Date = new Date("2024-01-01");
-  const memo: string = "テスト";
+  const testCreateTransactionComplex: CreateTransactionComplex & {
+    memberCount: number;
+  } = {
+    memberCount: 2,
 
-  const memberCount: number = 2;
-  const members: Member[] = [
-    {
-      userId: 1,
-      finalBill: 1000,
-      balance: 0.5,
-    },
-    {
-      userId: 2,
-      finalBill: 0,
-      balance: 0.5,
-    },
-  ];
+    creatorId: 1,
+    groupId: 1,
+    status: "PENDING",
+    method: "RATIO",
+    amount: 1000,
+    categoryId: 1,
+    title: "テスト",
+    paymentDate: new Date("2024-01-01"),
+    memo: "テスト",
+    member: [
+      {
+        userId: 1,
+        finalBill: 1000,
+        balance: 0.5,
+      },
+      {
+        userId: 2,
+        finalBill: 0,
+        balance: 0.5,
+      },
+    ],
+    ...overrides,
+  };
 
-  formData.append("method", method.toString());
-  formData.append("amount", amount.toString());
-  formData.append("categoryId", categoryId.toString());
-  formData.append("title", title.toString());
-  formData.append("paymentDate", paymentDate.toString());
-  formData.append("memo", memo.toString());
-  formData.append("memberCount", memberCount.toString());
-  members.forEach((member, index) => {
+  formData.append(
+    "memberCount",
+    testCreateTransactionComplex.memberCount.toString(),
+  );
+
+  formData.append("method", testCreateTransactionComplex.method.toString());
+  formData.append("amount", testCreateTransactionComplex.amount.toString());
+  formData.append(
+    "categoryId",
+    testCreateTransactionComplex.categoryId.toString(),
+  );
+  formData.append("title", testCreateTransactionComplex.title.toString());
+  formData.append(
+    "paymentDate",
+    testCreateTransactionComplex.paymentDate.toString(),
+  );
+  formData.append("memo", testCreateTransactionComplex.memo.toString());
+  testCreateTransactionComplex.member.forEach((member, index) => {
     Object.entries(member).forEach(([key, value]) => {
       const formKey = `member.${index.toString()}.${key}`;
       const formValue = value.toString();
@@ -50,11 +66,11 @@ const createMock = (): FormData => {
 
 describe("extractFormData", () => {
   test("フォームに入力したデータオブジェクト形式で返ってくること", () => {
-    const formDataMock = createMock();
+    const formDataMock = createMock({ categoryId: 2 });
     const formValues = extractFormData(formDataMock);
     expect(formValues).toEqual({
       amount: 1000,
-      categoryId: 1,
+      categoryId: 2,
       title: "テスト",
       method: "RATIO",
       paymentDate: expect.any(Date),
