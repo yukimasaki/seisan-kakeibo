7e4366de01335e1f92e277bd89b9973b4105622e
yukimasaki <woodenhoe@gmail.com>
2024-03-21 11:45:34 +0000

    fix: pm2の起動エラー対策

--- project/backend/ecosystem.config.cjs
+++ project/backend/ecosystem.config.cjs
@@ -4,10 +4,10 @@ module.exports = {
       name: 'nest',
       exec_mode: 'cluster',
       instances: '-1',
-      script: './dist/src/main.js',
+      script: './backend/dist/src/main.js',
       args: 'start',
       time: true,
       log_date_format: 'YYYY-MM-DD HH:mm Z',
     },
   ],
-}
+};
